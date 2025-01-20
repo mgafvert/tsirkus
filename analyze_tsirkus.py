a7d0869fb2acea6474c1d0f7d7f306cf6ea6a205
Magnus Gäfvert <magnus.gafvert@gmail.com>
2025-01-20 18:36:41 +0100

    analysis script with probability distribution plots and percentile calculations

--- analyze_tsirkus.py
+++ analyze_tsirkus.py
@@ -3,16 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#t = ts.Tsirkus(jumps=[])
 t = ts.Tsirkus()
 p0 = np.eye(t.N,1)
-p_final = 0.99
+p_final = 0.999
 P = np.squeeze(np.array([p for p in t.evolve_final(p0, p_final)]))
 p_end = P[:,-1]
-f1 = plt.figure()
-plt.plot(p_end)
-f1.show()
 p_end_diff = np.diff(p_end,prepend=0.)
-f2 = plt.figure()
+x = np.arange(len(p_end))
+f1 = plt.figure(1)
+plt.plot(p_end)
+plt.grid(visible=True)
+f1.show()
+
+f2 = plt.figure(2)
 plt.plot(p_end_diff)
+plt.grid(visible=True)
 f2.show()
+
+i = np.where(p_end > 0.)[0][0] # first [0] to pick the array from the tuple, second [0] to pick the first element
+print(f'Minimum turns to win: {i} (probability {p_end[i]:.3e})')
+
+i = np.argmax(p_end_diff)
+print(f'Nunber of turns with highest probability to win: {i} (probability {p_end_diff[i]:.3e})')
+
+percentiles = [0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99, 0.999]
+for p in percentiles:
+    i = np.where(p_end <= p)[0][-1]
+    print(f'Percentile {p:.2f} at turn {i} (probability {p_end[i]:.3e})')
 
